7c30c8190e623329c7b6e4a8614067ef195b8c51
nintharray <167829823+nintharray@users.noreply.github.com>
2024-06-02 08:13:35 -0400

    refactor: change gpio configuration

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -24,33 +24,33 @@ size_t backend_count;
 KeyboardMode *current_kb_mode = nullptr;
 
 GpioButtonMapping button_mappings[] = {
-    {&InputState::l,            5 },
-    { &InputState::left,        4 },
-    { &InputState::down,        3 },
-    { &InputState::right,       2 },
+    {&InputState::l,            19},
+    { &InputState::left,        8 },
+    { &InputState::down,        7 },
+    { &InputState::right,       6 },
 
-    { &InputState::mod_x,       6 },
-    { &InputState::mod_y,       7 },
+    { &InputState::mod_x,       4 },
+    { &InputState::mod_y,       5 },
 
-    { &InputState::select,      10},
-    { &InputState::start,       0 },
+    { &InputState::select,      18}, // unused
+    { &InputState::start,       12},
     { &InputState::home,        11},
 
-    { &InputState::c_left,      13},
+    { &InputState::c_left,      21},
     { &InputState::c_up,        12},
-    { &InputState::c_down,      15},
-    { &InputState::a,           14},
-    { &InputState::c_right,     16},
+    { &InputState::c_down,      20},
+    { &InputState::a,           1 },
+    { &InputState::c_right,     26},
 
-    { &InputState::b,           26},
-    { &InputState::x,           21},
-    { &InputState::z,           19},
-    { &InputState::up,          17},
+    { &InputState::b,           2 },
+    { &InputState::x,           9 },
+    { &InputState::z,           0 },
+    { &InputState::up,          10},
 
     { &InputState::r,           27},
-    { &InputState::y,           22},
-    { &InputState::lightshield, 20},
-    { &InputState::midshield,   18},
+    { &InputState::y,           3 },
+    { &InputState::lightshield, 17}, // unused
+    { &InputState::midshield,   16}, // unused
 };
 size_t button_count = sizeof(button_mappings) / sizeof(GpioButtonMapping);
 
